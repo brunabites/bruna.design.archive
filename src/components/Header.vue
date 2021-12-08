af2d89a07effea0c266354c1c0ad0af3b4f71df7
Bruna Bites <bruna.bites@gmail.com>
2021-12-08 17:23:36 -0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,20 +29,6 @@ export default {
 }
 </script>
 
-<script>
-export default {
-  mounted() {
-    const plugin = document.createElement("script");
-    plugin.setAttribute(
-      "src",
-      "https://www.googletagmanager.com/gtag/js?id=G-B7B4SVX90B"
-    );
-    plugin.async = true;
-    document.head.appendChild(plugin);
-  }
-};
-</script>
-
 <style scoped>
 .header {
     position: relative;
